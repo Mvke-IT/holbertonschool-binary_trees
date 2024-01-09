a4e05033518990a5002e320f154022afc59ddac4
Mvke-IT <michael.duarte09@gmail.com>
2024-01-09 14:37:14 -0400

    fixed declarations

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,16 +1,24 @@
 #include "binary_trees.h"
-// binary_tree_node - creates a binary tree node
-// @parent: pointer to the parent node of the node to create
-// @value: value to put in the new node
-// Return: pointer to the new node, or NULL on failure
-
+/**
+ * binary_tree_node - function that creates a binary tree node
+ *
+ * @parent: is a pointer to the parent node of the node to create
+ * @value: is the value to put in the new node
+ *
+ * Return: a pointer to the new node or NULL on failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = NULL;
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = value;
-    new_node->parent = parent;
-    return (new_node);
+	binary_tree_t *newNode;
+
+	newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->parent = parent;
+	return (newNode);
 }
