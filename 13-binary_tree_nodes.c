453d036cdc2ed46979be47fdebef1b45e73f5bc5
Mvke-IT <michael.duarte09@gmail.com>
2024-01-10 16:22:18 -0400

    fixed declarations

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,19 +1,17 @@
 #include "binary_trees.h"
 /**
- * binary_tree_leaves - function that counts the leaves in a binary tree
+ * binary_tree_leaves - function that counts the nodes in a binary tree
  *
- * @tree: is a pointer to the root node of the tree to count the number of leaves
+ * @tree: is a pointer to the root node of the tree
  *
  * Return: number of leaves in a binary tree or 0 if tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count = 0;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left != NULL || tree->right != NULL)
-		count += 1;
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) +
-count);
+    if (tree == NULL)
+        return (0);
+    else if (tree->left == NULL && tree->right == NULL)
+        return (1);
+    else
+        return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
